e955a64b8d94339d09d22bfa20d4cb517f4df6ae
Solano Rodrigues <rodrigues.solano@gmail.com>
2024-06-06 18:59:00 +0000

    Atualizando Diretorio

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  #version = "3.5.0"
-  credentials = file ("tech-prod-419500-14ee6d9560be.json")
-  project = "poised-beach-420818"
-  region = "us-east1" 
-  zone = "us-east1-c"
-}
-
